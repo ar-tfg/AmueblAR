32f655b0f4e280f0f744b3904c4b371d226681f9
ColinCop <colincop@ucm.es>
2019-09-16 01:57:14 +0200

    añadido al repo

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/AnimationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    //The object that contains this behaviour manages the sofa animations
+    private Animator sofaAnim;
+
+    //Functions that manage animations
+    public void setForward() {
+        sofaAnim.SetBool("Forward", true);
+        sofaAnim.SetBool("Backward", false);
+    }
+    public void setBackward() {
+        sofaAnim.SetBool("Forward", false);
+        sofaAnim.SetBool("Backward", true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sofaAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
---
+++ Assets/RotateLeft.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotateLeft : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private GameObject sofa;
+    private bool move = false;
+
+    public void setSofa(GameObject s) { sofa = s; }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        move = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        move = false;
+    }
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move)
+            sofa.transform.Rotate(Vector3.up);
+    }
+}
